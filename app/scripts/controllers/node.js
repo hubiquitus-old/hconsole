b1ed6ea8b3d9d806ce41151959867dc0fb821fcb
Guillaume Livron <guillaume.livron@gmail.com>
2013-05-15 16:10:48 +0200

    remove peer on peer-stop message

--- app/scripts/controllers/node.js
+++ app/scripts/controllers/node.js
@@ -5,8 +5,27 @@ angular.module('hconsoleApp').controller('NodeCtrl', function ($rootScope, $loca
         $location.path('/');
     }
 
-    var pIndexes = {};
     $scope.processes = [];
+
+    function findProcess(pid) {
+        for (var i = 0; i < $scope.processes.length; i++) {
+            var process = $scope.processes[i];
+            if (process.pid === pid) {
+                return process;
+            }
+        }
+        return null;
+    }
+
+    function findActor(process, id) {
+        for (var i = 0; i < process.actors.length; i++) {
+            var actor = process.actors[i];
+            if (actor.id === id) {
+                return actor;
+            }
+        }
+        return null;
+    }
 
     hubiquitus.onMessage(function (hMessage) {
         if (hMessage.type === 'peer-info') {
@@ -14,13 +33,11 @@ angular.module('hconsoleApp').controller('NodeCtrl', function ($rootScope, $loca
 
             var now = new Date().getTime();
 
-            if (typeof pIndexes[peerInfo.peerPID] === 'undefined') {
-                pIndexes[peerInfo.peerPID] = $scope.processes.length;
-                $scope.processes.push({indexes: {}, actors: []});
+            var process = findProcess(peerInfo.peerPID);
+            if (!process) {
+                process = {pid: peerInfo.peerPID, actors: []};
+                $scope.processes.push(process);
             }
-
-            var process = $scope.processes[pIndexes[peerInfo.peerPID]];
-            process.pid = peerInfo.peerPID;
             process.memory = peerInfo.peerMemory;
             process.loadAvg = peerInfo.peerLoadAvg;
 
@@ -31,16 +48,23 @@ angular.module('hconsoleApp').controller('NodeCtrl', function ($rootScope, $loca
                 process.heapSeries.addPoint([now, Math.round(((100 / peerInfo.peerMemory.heapTotal) * peerInfo.peerMemory.heapUsed) * 100) / 100], true, process.heapSeries.data.length > 500);
             }
 
-            if (typeof process.indexes[peerInfo.peerId] === 'undefined') {
-                process.indexes[peerInfo.peerId] = process.actors.length;
-                process.actors.push({});
+            var actor = findActor(process, peerInfo.peerId);
+            if (!actor) {
+                actor = {id: peerInfo.peerId};
+                process.actors.push(actor);
             }
-
-            var actor = process.actors[process.indexes[peerInfo.peerId]];
-            actor.id = peerInfo.peerId;
             actor.status = peerInfo.peerStatus;
-        } else {
-            console.warn(hMessage.type);
+        }
+        else if (hMessage.type === 'peer-stop') {
+            for (var i = 0; i < $scope.processes.length; i++) {
+                var p = $scope.processes[i];
+                for (var j = 0; j < p.actors.length; j++) {
+                    var a = p.actors[j];
+                    if (hMessage.payload.indexOf(a.id) === 0) {
+                        p.actors.splice(j, 1);
+                    }
+                }
+            }
         }
     });
 
